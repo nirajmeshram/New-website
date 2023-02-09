02ef7772d7ae623cb85b90ef7b53ff86a367ab25
nmeshram <niroj.meshram@sofmen.com>
2023-02-09 14:24:06 +0530

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,11 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index(){
+<<<<<<< Updated upstream
         $test ="This text is from main branch";
+=======
+       $h = "Hello world";
+>>>>>>> Stashed changes
         return "Hello world from test";
     }
 }
